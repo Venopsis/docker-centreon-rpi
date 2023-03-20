3b4adc52ab0b225e4a992f303504c6b4421ea9f2
Venopsis <949573+Venopsis@users.noreply.github.com>
2023-03-20 15:18:18 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "--------"
+echo "Running the container" $(hostname)
+echo "Time info :" $(date)
+echo "--------"
+if [ "$1" = "install" ]
+    then
+        # Run the installation script.
+        echo $(date) "- Installation mode initiated"
+        /centreon_central.sh
+elif [ "$1" = "start" ]
+    then
+        echo $(date) "- Centreon service : starting"
+        echo $(date) "- Centreon service : running"
+        # Command below keep container running after tmux command done
+        tail -f /dev/null
+else
+    echo $(date) "- !! ERROR !! This container does not accept other arguments than 'init' and 'start'"
+    exit 1
+fi
